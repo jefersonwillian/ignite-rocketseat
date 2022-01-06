762ffd611f47bf01d42ed6f62b67abdf123d3bcd
Jeferson Carvalho <jeferson.carvalho@cadmus.com.br>
2022-01-06 12:52:53 -0300

    First React component

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-    return <div><h1>Jeferson carvalho</h1></div>;
+    return <RepositoryList></RepositoryList>
 }
---
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,34 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="3">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="3">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="3">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="3">Acessar repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
