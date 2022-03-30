8b5191e53d7ec9181b34e79a4aa004a0a99b5b8e
Jeferson Carvalho <jefersonwilliansk8@hotmail.com>
2022-03-29 22:08:10 -0300

    creating the interfaces and typing the variables

--- reactjs/01-github-explorer/src/components/RepositoryItem.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-export function RepositoryItem(props) {
+interface IRepositoryItemProps{
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function RepositoryItem(props: IRepositoryItemProps) {
     return (
         <li>
             <strong>{props.repository?.name ?? 'Default'}</strong>
--- reactjs/01-github-explorer/src/components/RepositoryList.tsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
+interface IRepository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<IRepository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos').then(response => response.json()).then(data => setRepositories(data))
@@ -14,8 +20,8 @@ export function RepositoryList() {
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                {repositories.map((item) => {
-                    return <RepositoryItem key={item.name} repository={item} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
                 })},
             </ul>
         </section>
