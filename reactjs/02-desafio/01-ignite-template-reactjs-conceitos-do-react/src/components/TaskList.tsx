2e885adb7bd329ebf82fbc8a39bb7309955bc0a5
Jeferson Carvalho <jefersonwilliansk8@hotmail.com>
2022-03-30 00:41:50 -0300

    adjustments to validate empty field

--- reactjs/02-desafio/01-ignite-template-reactjs-conceitos-do-react/src/components/TaskList.tsx
+++ reactjs/02-desafio/01-ignite-template-reactjs-conceitos-do-react/src/components/TaskList.tsx
@@ -16,6 +16,9 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (!newTaskTitle) return;
+    
     const newTask = {
       id: Math.floor(Math.random() * 256),
       title: newTaskTitle,
