03f9a8b6f3b7f7f190c012555e965ca56e29e8c1
Jeferson Carvalho <jefersonwilliansk8@hotmail.com>
2022-03-29 21:50:29 -0300

    setting typescript in project in react

--- reactjs/01-github-explorer/babel.config.js
+++ reactjs/01-github-explorer/babel.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    presets: [
-        '@babel/preset-env',
-        ['@babel/preset-react', {
-            runtime: 'automatic'
-        }]
-    ]
-}
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
+};
